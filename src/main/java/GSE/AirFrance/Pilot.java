5f3044287e5bdd732822612360b3892ffe0e6978
Sergej Bakanow <s.bakanow@gmail.com>
2021-10-02 10:37:40 +0200

    Moved Pilot class

--- src/main/java/GSE/AirFrance/Pilot.java
+++ src/main/java/GSE/AirFrance/Pilot.java
@@ -1,4 +1,56 @@
 package GSE.AirFrance;
 
+import java.util.HashSet;
+
+/**
+ * @author Sergej Bakanow
+ */
+
 public class Pilot {
+
+  private String name;
+  private Airline employer;
+  private Set<Flight> assignments = new HashSet<>();
+
+  public Pilot(String name) {
+    this.name = name;
+    System.out.println(this + " created");
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Airline getEmployer() {
+    return employer;
+  }
+
+  public Set<Flight> getAssignments(){
+    return assignments;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setEmployer(Ariline employer) {
+    this.employer = employer;
+  }
+
+  public void addAssignment(Flight assignment) {
+    assignments.add(assignment);
+  }
+
+  public String toString() {
+    return "<" + this.getClass().getSimpleName() + "> " + "<" + getName() + ">";
+  }
+
+  public void hire() {
+    System.out.println(this + " is hired");
+  }
+
+  public void fire() {
+    System.out.println(this + " is fired");
+  }
 }
+
