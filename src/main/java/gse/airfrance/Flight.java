3faf73be3da07da834dbf293b97c80e373287b8e
Marvin Simon <simon.marvin.1@web.de>
2021-10-02 22:56:00 +0200

    chore: Added comments to the methods

--- src/main/java/gse/airfrance/Flight.java
+++ src/main/java/gse/airfrance/Flight.java
@@ -36,6 +36,7 @@ public class Flight {
   }
 
   // Begin of the getters
+
   public Plane getResource() {
     return resource;
   }
@@ -81,14 +82,33 @@ public class Flight {
     this.resource = resource;
   }
 
+  /**
+   * Set the captain of the flight.
+   *
+   * @param captain Set the captain of the flight
+   */
   public void setCaptain(Pilot captain) {
     this.captain = captain;
   }
 
+  /**
+   * Set the airline which is advertising the flight.
+   *
+   * @param theAirline Airline which advertised the flight
+   */
   public void setTheAirline(Airline theAirline) {
     this.theAirline = theAirline;
   }
 
+  /**
+   * Add origin airports to the flight.
+   * There can be more than one origin airport because stopovers are possible.
+   * There can be at max 5 stop overs.
+   * The stopover must be a valid airport, not a null reference.
+   *
+   * @param origin Origin Airport
+   * @return True if the origin was successfully added.
+   */
   public boolean addOrigin(Airport origin) {
     boolean result = false;
     if(origin != null && currentNumberOrigins < MAX_NUMBER_STOPS) {
@@ -99,6 +119,15 @@ public class Flight {
     return result;
   }
 
+  /**
+   * Add destination airports to the flight.
+   * There can be more than one destination airport because stopovers are possible.
+   * There can be at max 5 stop overs.
+   * The stopover must be a valid airport, not a null reference.
+   *
+   * @param destination Destination Airport
+   * @return True if the destination was successfully added.
+   */
   public boolean addDestination(Airport destination) {
     boolean result = false;
     if(destination != null && currentNumberDestinations < MAX_NUMBER_STOPS) {
@@ -109,6 +138,16 @@ public class Flight {
     return result;
   }
 
+  /**
+   * Every plane has a different capacity of passengers, but with the AIRBUS370 we got a maximum of 853.
+   * This method can be used to add a passenger to the flight.
+   * There can be no more than 853 passengers.
+   * There is no check with the machine used if max capacity is reached/over capacity.
+   * There can be no passanger added with a null reference.
+   *
+   * @param passenger Passenger to be added (one at a time)
+   * @return True if the action was successful, else false.
+   */
   public boolean addPassenger(Passenger passenger) {
     boolean result = false;
     if(passenger != null && currentNumberPassengers < MAX_NUMBER_PASSENGERS) {
@@ -119,6 +158,14 @@ public class Flight {
     return result;
   }
 
+  /**
+   * Every flight needs at least one co-pilot but at most 2.
+   * This function is to add the co-pilots.
+   * No null references possible.
+   *
+   * @param coPilot Co-Pilot to be added.
+   * @return True if the action was successful, else false
+   */
   public boolean addCoPilot(Pilot coPilot) {
     boolean result = false;
     if(coPilot != null && currentNumberCoPilots < MAX_NUMBER_CO_PILOTS) {
@@ -129,6 +176,11 @@ public class Flight {
     return result;
   }
 
+  /**
+   * Method sends back a String of the flight with the number and date it will be performed.
+   *
+   * @return String of the flight
+   */
   public String toString() {
     return this.getClass().getSimpleName() + " number "+ flightNum + " starting at " + date.toString();
   }
@@ -137,6 +189,11 @@ public class Flight {
     this.delayed = delayed;
   }
 
+  /**
+   * Prints the flight and a note if the flight is delayed or in time/schedule.
+   *
+   * @return Returns the value of delayed
+   */
   public boolean delayed() {
     String out = this.toString();
     out += delayed ? " is delayed!" : " is not delayed!";
