357f5010ebda9cfd06bee4badd97dd175b482126
Sergej Bakanow <s.bakanow@gmail.com>
2021-10-02 18:29:46 +0200

    Added some more tests

--- src/test/java/gse/airfrance/TestFlight.java
+++ src/test/java/gse/airfrance/TestFlight.java
@@ -11,14 +11,37 @@ public class TestFlight {
 
     Flight flight1;
     Date date = new Date();
+    Pilot captain;
+    Pilot firstCoPilot;
+    Pilot secondCoPilot;
+    Pilot thirdCoPilot;
     @Before
     public void setup() {
         flight1 = new Flight(date, (short) 5);
+        captain = new Pilot("John Doe");
+        firstCoPilot = new Pilot("Jane Doe");
+        secondCoPilot = new Pilot("James Dodo");
+        thirdCoPilot = new Pilot("Too many");
     }
 
     @Test
     public void PilotCreationTest() {
         Assert.assertNotNull(flight1);
+        flight1.setCaptain(captain);
+        Assert.assertNotNull(captain);
+        Assert.assertEquals(captain, flight1.getCaptain());
+        Assert.assertEquals("John Doe", captain.getName());
+    }
+    
+    @Test
+    public void CoPilotCreationTest() {
+        Assert.assertNotNull(flight1);
+        flight1.addCoPilot(firstCoPilot);
+        flight1.addCoPilot(secondCoPilot);
+        flight1.addCoPilot(thirdCoPilot);
+        Assert.assertNotNull(firstCoPilot);
+        Assert.assertNotNull(secondCoPilot);
+        Assert.assertEquals(2, flight1.getCoPilots().length);
     }
 
     @Test @Ignore
