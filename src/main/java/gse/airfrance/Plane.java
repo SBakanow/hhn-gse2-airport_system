ce7d0f7499924c6c3f8314b8b4d459a099775f5a
Emin Hallaceli <ehallaceli@stud.hs-heilbronn.de>
2021-10-03 23:27:27 +0200

    Began implementing the plane class (constructor, fields)

--- src/main/java/gse/airfrance/Plane.java
+++ src/main/java/gse/airfrance/Plane.java
@@ -1,4 +1,26 @@
 package gse.airfrance;
 
+/**
+ * Class that represents a plane in the air transportation planning system.
+ *
+ * @author Emin Hallaceli
+ */
 public class Plane {
+
+  private final String model;
+  private final String serialNum;
+  private final String tailNr;
+
+  private double hoursFlown;
+
+  /**
+   * Constructor for a plane.
+   */
+  public Plane(final String model, final String serialNum, final String tailNr) {
+    this.model = model;
+    this.serialNum = serialNum;
+    this.tailNr = tailNr;
+    hoursFlown = 0;
+  }
+
 }
