cb4ecd19723346fa3016456344a53bee174c145c
Dennis Schneider <dschneider@stud.hs-heilbronn.de>
2021-10-05 10:55:49 +0200

    added raw code for seat class

--- src/main/java/gse/airfrance/Seat.java
+++ src/main/java/gse/airfrance/Seat.java
@@ -1,4 +1,37 @@
 package gse.airfrance;
 
+/**
+ * Seat class for AirFrance Project
+ * @author Dennis Schneider
+ */
+
 public class Seat {
+
+  final String location;
+  boolean reserved = false;
+
+  public Seat(String location) {
+    this.location = location;
+    System.out.println(this + " created");
+  }
+
+  /**
+   * Getter Class for the location of the Seat
+   * @return Seat ID
+   */
+  public String getLocation() {
+    return location;
+  }
+
+  @Override
+  public String toString() {
+    return this.getClass().getSimpleName() + " " + getLocation();
+  }
+
+  /**
+   * Marks a seat as reserved
+   */
+  public void reserveSeat() {
+    reserved = true;
+  }
 }
